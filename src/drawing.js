1930e5574761f982f878f6299f133a9cf30f38e1
Elodie Moret <elodiewebdesigner@gmail.com>
2020-03-19 09:23:36 +0100

    Create dossier src

---
+++ src/drawing.js
@@ -0,0 +1,54 @@
+export default class Drawing {
+    static gameOver(ctx, centreX, centreY) {
+        ctx.save();
+        ctx.font = "70px sans-serif";
+        ctx.fillStyle = "#000";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 5;
+        ctx.strokeText("Game Over", centreX, centreY - 180);
+        ctx.fillText("Game Over", centreX, centreY - 180);
+        ctx.font = "bold 30px sans-serif";
+        ctx.strokeText("Appuyer sur la touche Espace pour rejouer", centreX, centreY - 120);
+        ctx.fillText("Appuyer sur la touche Espace pour rejouer", centreX, centreY - 120);
+        ctx.restore();
+    };
+
+    static drawScore(ctx, centreX, centreY, score) {
+        ctx.save();
+        ctx.font = "bold 150px sans-serif";
+        ctx.fillStyle = "#696969";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(score.toString(), centreX, centreY);
+        ctx.restore();
+    };
+
+    static drawSnake(ctx, blockSize, snake) {
+        ctx.save();
+        ctx.fillStyle = "rgb(30,144,255)";
+        for (let block of snake.body) {
+            this.drawBlock(ctx, block, blockSize);
+        }
+        ctx.restore();
+    }
+
+    static drawApple(ctx, blockSize, apple) {
+        const radius = blockSize / 2;
+        const x = apple.position[0] * blockSize + radius;
+        const y = apple.position[1] * blockSize + radius;
+        ctx.save();
+        ctx.fillStyle = "rgb(0,0,128)";
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.restore();
+    }
+
+    static drawBlock(ctx, position, blockSize) {
+        const x = position[0] * blockSize;
+        const y = position[1] * blockSize;
+        ctx.fillRect(x, y, blockSize, blockSize);
+    };
+}
